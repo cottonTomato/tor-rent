70886fafa88797d6975b93c4e88efef0379fb2cc
Abhinav Pandey <abhinav.pandey.1512@gmail.com>
2025-02-08 17:11:18 +0530

    rent_contract

--- anchor/programs/torrent/src/lib.rs
+++ anchor/programs/torrent/src/lib.rs
@@ -4,19 +4,69 @@ use anchor_lang::prelude::*;
 
 declare_id!("Au1tsG1thDEY9gaGdh7CPDUbEE8J3G5ykBGQjQdD6vzt");
 
+pub const ANCHOR_DISCRIMINATOR_SIZE: usize = 8;
+
 #[program]
 pub mod torrent {
     use super::*;
 
-    pub fn create_agreement(ctx: Context<CreateAgreement>) -> Result<()> {
+    pub fn create_agreement(
+        ctx: Context<CreateAgreement>,
+        rent_amount: u64,
+        deposit_amount: u64,
+        duration_months: u8,
+        ipfs_cid: String,
+    ) -> Result<()> {
+        ctx.accounts.rental_agreement.set_inner(RentalAgreement {
+            landlord: ctx.accounts.landlord.key(),
+            tenant: ctx.accounts.tenant.key(),
+            rent_amount,
+            deposit_amount,
+            duration_months,
+            ipfs_cid,
+            is_active: true,
+        });
+
         Ok(())
     }
 
-    pub fn update_agreement(ctx: Context<UpdateAgreement>) -> Result<()> {
+    pub fn update_agreement(
+        ctx: Context<UpdateAgreement>,
+        rent_amount: Option<u64>,
+        deposit_amount: Option<u64>,
+        duration_months: Option<u8>,
+        ipfs_cid: Option<String>,
+    ) -> Result<()> {
+        let rental_agreement = &mut ctx.accounts.rental_agreement;
+
+        if let Some(amount) = rent_amount {
+            rental_agreement.rent_amount = amount;
+        }
+        if let Some(amount) = deposit_amount {
+            rental_agreement.deposit_amount = amount;
+        }
+        if let Some(duration) = duration_months {
+            rental_agreement.duration_months = duration;
+        }
+        if let Some(cid) = ipfs_cid {
+            rental_agreement.ipfs_cid = cid;
+        }
+
         Ok(())
     }
 
     pub fn terminate_agreement(ctx: Context<TerminateAgreement>) -> Result<()> {
+        let rental_agreement = &mut ctx.accounts.rental_agreement;
+        let tenant = &ctx.accounts.tenant;
+
+        rental_agreement.is_active = false;
+        let remaining_deposit = rental_agreement.deposit_amount;
+
+        **tenant.try_borrow_mut_lamports()? += remaining_deposit;
+        **rental_agreement
+            .to_account_info()
+            .try_borrow_mut_lamports()? -= remaining_deposit;
+
         Ok(())
     }
 }
@@ -36,7 +86,7 @@ pub struct RentalAgreement {
 
 #[derive(Accounts)]
 pub struct CreateAgreement<'info> {
-    #[account(init, payer = landlord, space = 8 + RentalAgreement::INIT_SPACE)]
+    #[account(init, payer = landlord, space = ANCHOR_DISCRIMINATOR_SIZE + RentalAgreement::INIT_SPACE)]
     pub rental_agreement: Account<'info, RentalAgreement>,
     #[account(mut)]
     pub landlord: Signer<'info>,
